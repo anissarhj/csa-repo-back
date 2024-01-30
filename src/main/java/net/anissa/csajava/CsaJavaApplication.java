9046c3bdf6b6dea16d077bb372039dcde0fc87c1
Anissa Ras <anissa.rasoanirinahj@gmail.com>
2024-01-30 16:55:20 +0100

    first commit backend java

---
+++ src/main/java/net/anissa/csajava/CsaJavaApplication.java
@@ -0,0 +1,13 @@
+package net.anissa.csajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsaJavaApplication.class, args);
+    }
+
+}
